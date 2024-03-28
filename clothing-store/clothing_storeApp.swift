305fc061fe075ed32acfcf1486395f74b3d01e43
Mudara-Rajpaksha <144684836+Mudara-Rajpaksha@users.noreply.github.com>
2024-03-28 20:16:12 +0530

    Initial Commit

---
+++ clothing-store/clothing_storeApp.swift
@@ -0,0 +1,17 @@
+//
+//  clothing_storeApp.swift
+//  clothing-store
+//
+//  Created by Yesh Adithya on 2024-03-28.
+//
+
+import SwiftUI
+
+@main
+struct clothing_storeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
